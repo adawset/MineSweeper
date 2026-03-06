4fb96b6dd621fcde1ff29ea957c196a680b13f74
Andy <stdzhu@gmail.com>
2026-03-06 13:19:59 +0800

    chore: migrate to gradle daemon.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ pluginManagement {
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }
 }
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
 
 dependencyResolutionManagement {
     repositories {
